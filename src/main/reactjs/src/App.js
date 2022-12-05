87003135c82c05fb6339e072e763437213efd7d4
DESKTOP-JEAB2N0\to21 <kyungjin001>
2022-12-05 10:39:57 +0900

    UI 추가

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,33 +1,12 @@
-import React,{useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppRouter from './component/route/RouterComponent';
 
 function App() {
-    const [message, setMessage] = useState("");
-    useEffect(() => {
-        fetch('/main')
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-            });
-    }, [])
-    return (
-
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">{message}</h1>
-                <a
-                    className="App-link"
-                    href="https://www.naver.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
